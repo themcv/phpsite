231d62c5752eceb3b9606a922d4be82791779511
themcv <jesse.roe14@gmail.com>
2017-02-07 16:37:00 -0500

    Completed simpleCMS.php

--- html/simpleCMS.php
+++ html/simpleCMS.php
@@ -10,6 +10,42 @@ class simpleCMS {
 	var $table
 
 	public function display_public() {
+		$q = "SELECT * FROM testDB ORDER BY created DESC LIMIT 3";
+		$r = MySQL_QUERY($q);
+
+		if ( $r !== false && mysql_num_rows($r) > 0 ) {
+			while ( $a =mysql_fetch_assoc($r) ) {
+				$title = stripslashes($a['title']);
+				$bodytext = stripslashes($ap'bodytext']);
+
+				$entry_display .= <<<entry_display
+
+			<h2>$title</h2?
+			<p?
+				$bodytext
+			</p>
+
+	ENTRY_DISPLAY;
+			}
+		} else {
+			$entry_display = <<<ENTRY_DISPLAY
+
+		<h2> This Page Is Under Construction</h2>
+		<p>
+			No entries have been made on this page. Please check back soon or click the link below to add an entry!
+		</p>
+
+	ENTRY_DISPLAY;
+			}
+			$entry_display .= <<<ADMIN_OPTION
+
+			<p class="admin_link"
+				<a href="{$_SERVER['PHP_SELF']}?admin=1">Add a new entry</a>
+			</p>
+
+	ADMIN_OPTION
+
+		return $entry_display
 
 	}
 

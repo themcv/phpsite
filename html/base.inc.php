8b5959fccfd990633eccdb357f94fdb56f5cc097
Tom Elliott <tommygunsster@gmail.com>
2017-02-10 15:47:29 -0500

    Includes forgotten

---
+++ html/base.inc.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Instantiates our "needs" so a single
+ * reference will be needed instead of
+ * all of this information on all pages
+ * we create.
+ *
+ * PHP version 5
+ *
+ * category base.inc.php
+ * package  SimpleCMS
+ * author   THEMCV <nah@nah.com>
+ * @license linktolicense Simplenameforlicense
+ * @link    https://github.com/themcv/phpsite
+ */
+/**
+ * Instantiates our "needs" so a single
+ * reference will be needed instead of
+ * all of this information on all pages
+ * we create.
+ *
+ * category base.inc.php
+ * package  SimpleCMS
+ * author   THEMCV <nah@nah.com>
+ * @license linktolicense Simplenameforlicense
+ * @link    https://github.com/themcv/phpsite
+ */
+/**
+ * We need to pull in the configuration file.
+ */
+define('BASEPATH', dirname(__FILE__));
+if (!file_exists(BASEPATH.DIRECTORY_SEPARATOR.'config.inc.php')) {
+    printf(
+        '%s. %s. %s: %s%s%s %s: %s%s%s',
+        _('Configuration has not been setup'),
+        _('You need to create your config.inc.php'),
+        _('This can be done by editing'),
+        BASEPATH,
+        DIRECTORY_SEPARATOR,
+        'config.inc.php',
+        _('An example config file can be found at'),
+        BASEPATH,
+        DIRECTORY_SEPARATOR,
+        'config-example.inc.php'
+    );
+    exit;
+}
+require 'config.inc.php';
+/**
+ * Because you have to start somewhere, we must include our class first.
+ * In more complex setups you would want an "autoloader".
+ */
+require 'simpleCMS.php';
+/**
+ * Instantiate the simple cms class.
+ */
+$SimpleCMS = new SimpleCMS();
---
+++ html/create.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The index referenced.
+ *
+ * PHP version 5
+ *
+ * category Index
+ * package  SimpleCMS
+ * author   THEMCV <nah@nah.com>
+ * @license linktolicense Simplenameforlicense
+ * @link    https://github.com/themcv/phpsite
+ */
+/**
+ * The index referenced.
+ *
+ * category Index
+ * package  SimpleCMS
+ * author   THEMCV <nah@nah.com>
+ * @license linktolicense Simplenameforlicense
+ * @link    https://github.com/themcv/phpsite
+ */
+/**
+ * Common entry point.
+ */
+require dirname(__FILE__).DIRECTORY_SEPARATOR.'base.inc.php';
+/**
+ * Initial body information.
+ */
+$body = SimpleCMS::tag(
+    'body',
+    $SimpleCMS->displayAdmin()
+);
+/**
+ * Because of how I created the tag funciton we can call it outside the scope of the
+ * instantiated class object.
+ *
+ * To think about, however, is a simpler means to include tags maybe.
+ */
+echo SimpleCMS::tag(
+    'html',
+    $body
+);
